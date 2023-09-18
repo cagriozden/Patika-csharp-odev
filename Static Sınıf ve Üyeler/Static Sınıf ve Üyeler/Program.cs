02039d1867dd5e5cbd425d0df17714fd1a97d471
Çağrı Özden <cagri.ozden98@gmail.com>
2023-09-18 19:12:22 +0300

    Static Sınıf ve Üyeler
    Static Sınıf ve Üyeler

---
+++ Static Sınıf ve Üyeler/Static Sınıf ve Üyeler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Static_Sınıf_ve_Üyeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);
+            Calisan calisan = new Calisan("cagri","ozden","SE");
+            Console.WriteLine("Çalışan Sayısı:{0}", Calisan.CalisanSayisi);
+
+        }
+
+    }
+
+
+    class Calisan
+    {
+
+        private static int calisanSayisi;
+
+        public static int CalisanSayisi { get => calisanSayisi; }
+
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+
+        }
+
+
+        public Calisan(string isim, string soyisim, string departman)
+        {
+            this.Isim = isim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+
+    }
+}

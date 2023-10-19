43a511b28d31803fc766895a0dec41d943fc8913
Çağrı Özden <cagri.ozden98@gmail.com>
2023-10-19 17:45:47 +0300

    MovieStore

---
+++ Patika_Week_4MovieStore/MovieStoreApi/DbOperations/DataGenerator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MovieStoreApi.Entities;
+
+namespace MovieStoreApi.DbOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MovieStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<MovieStoreDbContext>>()))
+            {
+                if (context.Movies.Any())
+                    return;
+
+                context.Actors.AddRange(
+
+                    new Actor
+                    {
+                        Name = "Leonardo",
+                        Surname = "DiCaprio",
+                        Id = 1
+                    },
+                    new Actor
+                    {
+                        Name = "Tom",
+                        Surname = "Hanks",
+                        Id = 2
+                    },
+                    new Actor
+                    {
+                        Name = "Meryl",
+                        Surname = "Streep",
+                        Id = 3
+                    }
+                    );
+                context.Directors.AddRange(
+
+                    new Director
+                    {
+                        Name = "Christopher",
+                        Surname = "Nolan",
+                        Id = 1
+                    },
+                    new Director
+                    {
+                        Name = "Quentin",
+                        Surname = "Tarantino",
+                        Id = 2
+                    },
+                    new Director
+                    {
+                        Name = "Steven",
+                        Surname = "Spielberg",
+                        Id = 3
+                    }
+                    );
+                context.Genres.AddRange(
+
+                    new Genre
+                    {
+                        Name = "Drama"
+                    },
+                    new Genre
+                    {
+                        Name = "Action"
+                    },
+                    new Genre
+                    {
+                        Name = "Comedy"
+                    }
+                    );
+                context.SaveChanges();
+                context.Movies.AddRange(
+
+                    new Movie
+                    {
+                        Name = "Inception",
+                        Year = 2010,
+                        Actors = context.Actors.Where(c => new[] { 1, 2 }.Contains(c.Id)).ToList(),
+                        DirectorId = 1,
+                        GenreId = 1,
+                        Price = 50
+                    },
+                    new Movie
+                    {
+                        Name = "Pulp Fiction",
+                        Year = 1994,
+                        Actors = context.Actors.Where(c => new[] { 2, 3 }.Contains(c.Id)).ToList(),
+                        DirectorId = 2,
+                        GenreId = 2,
+                        Price = 45
+                    },
+                    new Movie
+                    {
+                        Name = "Forrest Gump",
+                        Year = 1994,
+                        Actors = context.Actors.Where(c => new[] { 1, 3 }.Contains(c.Id)).ToList(),
+                        DirectorId = 3,
+                        GenreId = 3,
+                        Price = 55
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Patika_Week_4MovieStore/MovieStoreApi/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieStoreApi.DbOperations;
+using System.Reflection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MovieStoreDbContext>(options => options.UseInMemoryDatabase(databaseName: "MovieStoreDB"));
+builder.Services.AddScoped<IMovieStoreDbContext>(c => c.GetService<MovieStoreDbContext>());
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DataGenerator.Initialize(services);
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+
+
+app.MapControllers();
+
+app.Run();
+
+
